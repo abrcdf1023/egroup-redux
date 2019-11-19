586ac0caf499b0bd526b53104fb669d52c1c42ab
SP_Penguin <abrcdf1023@gmail.com>
2019-11-19 16:02:31 +0800

    update dependencies and remove some build plugins

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
 module.exports = function(api) {
   if (api.env(['test'])) {
     return {
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-      plugins: [
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-proposal-class-properties'
-      ]
+      presets: ['@babel/preset-env', '@babel/preset-react']
     };
   }
 
@@ -21,8 +17,6 @@ module.exports = function(api) {
       '@babel/preset-react'
     ],
     plugins: [
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-syntax-dynamic-import',
       // A plugin that enables the re-use of Babel's injected helper code to save on codesize.
       // https://babeljs.io/docs/en/babel-plugin-transform-runtime
       '@babel/plugin-transform-runtime'

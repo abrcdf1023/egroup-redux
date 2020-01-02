1cd984a62fe5a245284d874148ce886d97adecd9
SP_Penguin <abrcdf1023@gmail.com>
2020-01-02 11:28:02 +0800

    withReduxDialog add displayName

--- packages/redux-modules/src/withReduxDialog/withReduxDialog.js
+++ packages/redux-modules/src/withReduxDialog/withReduxDialog.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import { getDialogStates, initializeDialog, closeDialog } from '../dialogs';
 
 const withReduxDialog = name => Dialog => {
-  class ReduxDialog extends Component {
+  class ReduxDialog extends React.Component {
     componentDidMount() {
       this.props.initializeDialog(name);
     }
@@ -32,9 +32,15 @@ const withReduxDialog = name => Dialog => {
    * Forwarding refs in higher-order components
    * https://reactjs.org/docs/forwarding-refs.html#forwarding-refs-in-higher-order-components
    */
-  const ForwardedComponent = React.forwardRef((props, ref) => {
+  function forwardRef(props, ref) {
     return <ReduxDialog {...props} forwardedRef={ref} />;
-  });
+  }
+
+  // Give this component a more helpful display name in DevTools.
+  const dialogComponentName = Dialog.displayName || Dialog.name;
+  forwardRef.displayName = `withReduxDialog(${dialogComponentName})`;
+
+  const ForwardedComponent = React.forwardRef(forwardRef);
 
   const mapStateToProps = (state, props) => ({
     ...getDialogStates(state, props, name).toJS()

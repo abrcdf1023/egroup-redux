1709d8951be14db7a1847c3f7c90f9831176e99e
SP_Penguin <abrcdf1023@gmail.com>
2020-01-02 11:43:03 +0800

    withReduxDialog fixed forwardRef

--- packages/redux-modules/src/withReduxDialog/withReduxDialog.js
+++ packages/redux-modules/src/withReduxDialog/withReduxDialog.js
@@ -29,30 +29,34 @@ const withReduxDialog = name => Dialog => {
   }
 
   /**
+   * Connect before forwardRef
+   * https://github.com/reduxjs/react-redux/issues/914
+   */
+  const mapStateToProps = (state, props) => ({
+    ...getDialogStates(state, props, name).toJS()
+  });
+
+  const ConnectedComponent = connect(
+    mapStateToProps,
+    {
+      initializeDialog,
+      closeDialog
+    }
+  )(ReduxDialog);
+
+  /**
    * Forwarding refs in higher-order components
    * https://reactjs.org/docs/forwarding-refs.html#forwarding-refs-in-higher-order-components
    */
   function forwardRef(props, ref) {
-    return <ReduxDialog {...props} forwardedRef={ref} />;
+    return <ConnectedComponent {...props} forwardedRef={ref} />;
   }
 
   // Give this component a more helpful display name in DevTools.
   const dialogComponentName = Dialog.displayName || Dialog.name;
   forwardRef.displayName = `withReduxDialog(${dialogComponentName})`;
 
-  const ForwardedComponent = React.forwardRef(forwardRef);
-
-  const mapStateToProps = (state, props) => ({
-    ...getDialogStates(state, props, name).toJS()
-  });
-
-  return connect(
-    mapStateToProps,
-    {
-      initializeDialog,
-      closeDialog
-    }
-  )(ForwardedComponent);
+  return React.forwardRef(forwardRef);
 };
 
 export default withReduxDialog;

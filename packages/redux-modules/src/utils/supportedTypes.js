189519940c56ac915acbdad29b7469c88db3377a
SP_Penguin <abrcdf1023@gmail.com>
2020-02-17 15:39:43 +0800

    supportedTypes add comment

--- packages/redux-modules/src/utils/supportedTypes.js
+++ packages/redux-modules/src/utils/supportedTypes.js
@@ -1,3 +1,8 @@
+/**
+ * Check variable has supported types(at least one of all).
+ * @param {any} variable
+ * @param {array} supportedTypes
+ */
 export default function supportedTypes(variable, supportedTypes) {
   let type = typeof variable;
   if (type === 'object') {

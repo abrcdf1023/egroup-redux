1b01193c9e0546ac7fdae34d00055b4259a99ac9
SP_Penguin <abrcdf1023@gmail.com>
2020-05-27 17:27:37 +0800

    utils add setIn

--- packages/redux-modules/src/utils/index.js
+++ packages/redux-modules/src/utils/index.js
@@ -4,3 +4,4 @@ export { default as trimLeafs } from './trimLeafs';
 export { default as supportedTypes } from './supportedTypes';
 export { default as findFetchIndex } from './findFetchIndex';
 export { default as getTrimedLeafs } from './getTrimedLeafs';
+export { default as setIn } from './setIn';

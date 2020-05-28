29cdbb9e90cf6ee057dfa700b37425412ce53ab7
SP_Penguin <abrcdf1023@gmail.com>
2020-05-28 11:38:46 +0800

    add immutable withReduxSnackbar

---
+++ packages/redux-modules/src/immutable/withReduxSnackbar/withReduxSnackbar.test.js
@@ -0,0 +1,49 @@
+import '@testing-library/jest-dom/extend-expect';
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { fromJS } from 'immutable';
+import configureStore from 'redux-mock-store';
+import withReduxSnackbar from './withReduxSnackbar';
+
+let store;
+const snackbarName = 'globalSnackbar';
+
+const MockSnackbar = ({ isOpen, title, message }) => {
+  return (
+    <div>
+      <p>{title}</p>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+describe('Redux Snackbar HOC', () => {
+  beforeEach(() => {
+    const mockStore = configureStore();
+
+    store = mockStore(
+      fromJS({
+        snackbars: {
+          [snackbarName]: {
+            isOpen: false,
+            title: 'snackbar title',
+            message: 'snackbar message'
+          }
+        }
+      })
+    );
+  });
+  it('Should render the component only when snackbar prop is true', () => {
+    const ReduxSnackbar = withReduxSnackbar(snackbarName)(MockSnackbar);
+    const { getByText } = render(<ReduxSnackbar store={store} />);
+    expect(getByText('snackbar title')).toBeInTheDocument();
+    expect(getByText('snackbar message')).toBeInTheDocument();
+  });
+  it('Should pass ref to Snackbar component', () => {
+    const ReduxSnackbar = withReduxSnackbar(snackbarName)(MockSnackbar);
+    const ref = React.createRef(null);
+    render(<ReduxSnackbar ref={ref} store={store} />);
+    expect(ref.current).not.toBeNull();
+  });
+});
